3f74dac7744f82167f60e1018c56d1143e6743f9
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-01-12 15:00:02 +0000

    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+import Link from 'next/link'
+
 function Home() {
-  return <h1>HomePage</h1>
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
+    </div>
+  )
 }
 export default Home
