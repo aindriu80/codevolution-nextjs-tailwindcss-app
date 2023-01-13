e406f67a10d9d06f663172dafebbc8764f2c2790
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-01-13 14:40:57 +0000

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order......')
+    router.replace('/product')
+  }
   return (
     <div>
       <h1>HomePage</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Products</Link>
+      <button onClick={handleClick}>Click me now!</button>
     </div>
   )
 }
