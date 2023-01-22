7704ba91640b7960de6899bfba6fe84d04eb36c8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-01-22 15:47:18 +0000

    Pico CSS

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,37 @@ function Home() {
   }
   return (
     <div>
-      <h1>HomePage</h1>
-      <Link href="/blog">Blog</Link>
-      <Link href="/product">Products</Link>
-      <Link href="/prerender">Pre Render</Link>
+      <header class="container">
+        <hgroup>
+          <h1>Next.JS HomePage</h1>
+          <h2>
+            a Next.JS project using some of it's features, with Pico CSS styling
+          </h2>
+        </hgroup>
+      </header>
+
+      <div className="container">
+        <section>
+          <Link href="/blog">Blog </Link> |
+          <Link href="/product"> Products </Link> |
+          <Link href="/prerender"> Pre Render </Link>
+        </section>
+      </div>
+
       <br />
-      <button onClick={handleClick}>Click me now!</button>
+
+      <div className="container">
+        <button onClick={handleClick} class="secondary">
+          Click me now!
+        </button>
+      </div>
       <br />
-      <Link href="/users">Users</Link>
-      <br />
-      <Link href="/posts">Posts</Link>
+
+      <div className="container">
+        <section>
+          <Link href="/users">Users </Link> |<Link href="/posts"> Posts</Link>
+        </section>
+      </div>
     </div>
   )
 }
