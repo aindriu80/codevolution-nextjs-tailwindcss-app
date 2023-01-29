3e057ac4ba5cfadeb97c037944f4e99a8696e69b
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-01-29 15:53:18 +0000

    API POST Request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -3,12 +3,26 @@ import Link from 'next/link'
 
 function CommentsPage() {
   const [comments, setComments] = useState([])
+  const [comment, setComment] = useState('')
 
   const fetchComments = async () => {
     const response = await fetch('/api/comments')
     const data = await response.json()
     setComments(data)
   }
+
+  const submitComment = async () => {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <>
       <header className="container">
@@ -20,6 +34,13 @@ function CommentsPage() {
       </header>
 
       <div className="container">
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Please enter a comment"
+        />
+        <button onClick={submitComment}>Submit Comment</button>
         <button onClick={fetchComments}>Load Comments</button>
         {comments.map((comment) => {
           return (
